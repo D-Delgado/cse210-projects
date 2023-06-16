b427be72a1c0901fe4dda1a4aa401268c436a553
D-Delgado <daniel.delgado1990@gmail.com>
2023-06-16 15:10:35 -0600

    Added code for Learning Activity - C# Prep 3 assignment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string play_again = "yes";
+        while (play_again == "yes")
+        {        
+            // User provided number logic
+            // Console.Write("What is the magic number? ");
+            // int answer = int.Parse(Console.ReadLine());
+            
+            // Logic for using random generator
+            Random randomGenerator = new Random();
+            int answer = randomGenerator.Next(1, 101);
+
+            int user_guess = -1;
+
+            int guess_count = 0;
+
+            while (user_guess != answer)
+            {
+                guess_count += 1;
+                Console.Write("What is your guess? ");
+                user_guess = int.Parse(Console.ReadLine());
+
+                if (answer > user_guess)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (answer < user_guess)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed it! It took you {guess_count} guesses!");
+                    Console.WriteLine("Want to play again? yes|no");
+                    play_again = Console.ReadLine();
+                }
+            }
+        }
     }
 }
